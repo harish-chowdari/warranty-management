30dacfc76f8f1ba6b4717a50f790fb3525fd3ae0
harish-nagubadi <Harish.N@veots.com>
2025-03-21 14:25:06 +0530

    axios update

--- admin/src/axios.js
+++ admin/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:4000/api"; 
+const BASE_URL = "https://warranty-management.onrender.com/api" 
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,
